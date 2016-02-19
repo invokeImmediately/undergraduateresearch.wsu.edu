e53580603a71d62fb9c6913cbf1e4e71bb2c7d97
Daniel Rieck <danielcrieck@gmail.com>
2016-02-19 15:43:24 -0800

    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -288,34 +288,38 @@
     "use strict";
     
 	$(document).ready(function () {
-        hghlghtRqrdInpts('.oue-gf-rqrd-input');
-        hghlghtRqrdChckbxs('.oue-gf-rqrd-checkbox');
-        hghlghtRqrdTxtAreas('.oue-gf-rqrd-txtarea');
-        setupActvtrChckbxs('.oue-gf-actvtr-checkbox');
+        if($("div.gform_body").length > 0) {
+            hghlghtRqrdInpts(".oue-gf-rqrd-input");
+            hghlghtRqrdChckbxs(".oue-gf-rqrd-checkbox");
+            hghlghtRqrdTxtAreas(".oue-gf-rqrd-txtarea");
+            setupActvtrChckbxs(".oue-gf-actvtr-checkbox");
+            setupActvtrChain(".oue-gf-actvtr-chain");
+            setupUploadChain(".oue-gf-upload-chain");
+        }
     });
     
     /******************************************************************************************\
-    | Via CSS, highlight required inputs until a value has been properly entered               |
+    | Highlight required INPUTS until a value has been properly entered                        |
     \******************************************************************************************/
     function hghlghtRqrdInpts (selector) {
         if ($.type(selector) === "string") {
             $(selector).each(function () {
                 var $this = $(this);
-                var $inputs = $this.find('input');
+                var $inputs = $this.find("input");
                 $inputs.each(function () {
                     var $thisChild = $(this);
                     if ($thisChild.val() == "") {
-                        $thisChild.removeClass('gf-value-entered');
+                        $thisChild.removeClass("gf-value-entered");
                     }
                     else {
-                        $thisChild.addClass('gf-value-entered');
+                        $thisChild.addClass("gf-value-entered");
                     }
                     $thisChild.blur(function () {
                         if ($thisChild.val() == "") {
-                            $thisChild.removeClass('gf-value-entered');
+                            $thisChild.removeClass("gf-value-entered");
                         }
                         else {
-                            $thisChild.addClass('gf-value-entered');
+                            $thisChild.addClass("gf-value-entered");
                         }
                     });
                 });
@@ -324,31 +328,31 @@
     }
 
     /******************************************************************************************\
-    | Via CSS, highlight required checkboxes until at least one has been checked               |
+    | Highlight required CHECKBOXES until at least one has been checked                        |
     \******************************************************************************************/
     function hghlghtRqrdChckbxs (selector) {
         if ($.type(selector) === "string") {
             $(selector).each(function () {
                 var $this = $(this);
-                var $inputs = $this.find('input');
+                var $inputs = $this.find("input");
                 $inputs.each(function () {
                     var $thisChild = $(this);
                     $thisChild.change(function () {
                         var $thisParent, $parentsInputs;
                         var inputReady = false;
                         
-                        $thisParent = $thisChild.parents('ul.gfield_checkbox');
-                        $parentsInputs = $thisParent.find('input');
+                        $thisParent = $thisChild.parents("ul.gfield_checkbox");
+                        $parentsInputs = $thisParent.find("input");
                         $parentsInputs.each(function () {
-                            if ($(this).prop('checked') == true && !inputReady) {
+                            if ($(this).prop("checked") == true && !inputReady) {
                                 inputReady = true;
                             }
                         });
                         if (inputReady) {
-                            $thisParent.addClass('gf-value-entered');
+                            $thisParent.addClass("gf-value-entered");
                         }
                         else {
-                            $thisParent.removeClass('gf-value-entered');
+                            $thisParent.removeClass("gf-value-entered");
                         }
                     });
                 });
@@ -357,27 +361,27 @@
     }
 
     /******************************************************************************************\
-    | Via CSS, highlight required text are inputs until a value has been properly entered      |
+    | Highlight required TEXT AREA inputs until a value has been properly entered              |
     \******************************************************************************************/
     function hghlghtRqrdTxtAreas (selector) {
         if ($.type(selector) === "string") {
             $(selector).each(function () {
                 var $this = $(this);
-                var $inputs = $this.find('textarea');
+                var $inputs = $this.find("textarea");
                 $inputs.each(function () {
                     var $thisChild = $(this);
                     if ($thisChild.val() == "") {
-                        $thisChild.removeClass('gf-value-entered');
+                        $thisChild.removeClass("gf-value-entered");
                     }
                     else {
-                        $thisChild.addClass('gf-value-entered');
+                        $thisChild.addClass("gf-value-entered");
                     }
                     $thisChild.change(function () {
                         if ($thisChild.val() == "") {
-                            $thisChild.removeClass('gf-value-entered');
+                            $thisChild.removeClass("gf-value-entered");
                         }
                         else {
-                            $thisChild.addClass('gf-value-entered');
+                            $thisChild.addClass("gf-value-entered");
                         }
                     });
                 });
@@ -390,15 +394,106 @@
     \******************************************************************************************/
     function setupActvtrChckbxs (selector) {
         if ($.type(selector) === "string") {
-            $('.gform_body').on('change', selector + ' input', function () {
+            $(".gform_body").on("change", selector + " input", function () {
                 var $thisChild = $(this);
                 var $thisParent = $thisChild.parents(selector);
-                $thisParent.addClass('gf-activated');
+                $thisParent.addClass("gf-activated");
             });
         }
     }
     
-})(jQuery);
+    /******************************************************************************************\
+    | Setup a chain of activator checkboxes, wherein once a checkbox is activated/deactivated, |
+    | only its closest previous sibling is hidden/shown.                                       |
+    \******************************************************************************************/
+    function setupActvtrChain (selector) {
+        if ($.type(selector) === "string") {
+            $(".gform_body").on("change", selector + " input", function () {
+                var $thisChild = $(this);
+                var $thisParent = $thisChild.parents(selector);
+                var $parentPrevSblngs = $thisParent.prevAll(selector);
+                if($thisChild.prop("checked")) {
+                    $parentPrevSblngs.first().addClass("gf-hidden");
+                }
+                else {
+                    $parentPrevSblngs.first().removeClass("gf-hidden");
+                }
+            });
+        }
+    }
+
+    /******************************************************************************************\
+    | Setup a chain of file uploading inputs, wherein only the left-most input in the tree is  |
+    | visible. As the user uploads files in sequence, the next nearest neighbor is unveiled.   |
+    \******************************************************************************************/
+    function setupUploadChain (selector) {
+        if ($.type(selector) === "string") {
+            /* CHECK IF UPLOADS ALREADY EXIST:
+             *  It is possible to arrive at this point in execution after the user has submitted a
+             *  form containing errors that also already contains transcripts uploaded to input
+             *  fields that will be hidden by default. The following blocks of code resolve this
+             *  situation by showing such fields, as well as their nearest neighbors.
+             */
+            var $inputs = $(selector + " input[type='file']");
+            $inputs.each(function () {
+                var $thisInput = $(this);
+                var $nextDiv = $thisInput.nextAll("div[id]").first();
+                if($nextDiv.length > 0) {
+                    $thisInput.addClass("gf-value-entered");
+                    var $parentOfInput = $thisInput.parents(selector).first();
+                    $parentOfInput.removeClass("gf-hidden");
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector).first();
+                    $parentNextSblngs.removeClass("gf-hidden");
+                }
+            });
+            $(".gform_body").on("change", selector + " input[type='file']", function () {
+                var $thisInput = $(this);
+                if($thisInput.prop("files") != null && $thisInput.prop("files").length > 0) {
+                    var valuePassed = true;
+                    var $parentOfInput = $thisInput.parents(selector).first();
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector);
+                    var $parentPrevSblngs = $parentOfInput.prevAll(selector);
+                    if($parentNextSblngs.length != 0 || $parentPrevSblngs.length != 0) {
+                        var originalFileName = $thisInput.prop("files").item(0).name;
+                        $parentPrevSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                        $parentNextSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                    }
+                    if(valuePassed) {                      
+                        $thisInput.addClass("gf-value-entered");
+                        $parentNextSblngs.first().removeClass("gf-hidden");
+                    }
+                    else
+                    {
+                        alert("A file with the same name has already been uploaded; please choose a different file.");
+                        $thisInput.get(0).value = "";
+                    }
+                }
+                else {
+                    $thisChild.removeClass("gf-value-entered");
+                }
+            });
+        }
+    }
+    
+ })(jQuery);
 /**********************************************************************************************************************
  JQUERY QTIP TOOL TIPS PLUGIN
  *********************************************************************************************************************/
