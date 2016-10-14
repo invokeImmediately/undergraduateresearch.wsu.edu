aefa27456a8c2863c82609b323479a16dc8c0a0b
Daniel Rieck <danielcrieck@gmail.com>
2016-10-14 12:24:50 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -632,7 +632,7 @@ function isJQuery($obj) {
     \****************************************************************************************************/
     function initWsuIdInputs(slctrInputs) {
         var $wsuIdInputs = $(slctrInputs).find("input[type='text']");
-		$wsuIdInputs.keyup(function(e) {
+		$wsuIdInputs.keydown(function(e) {
             var $this = $(this);
             var inputText = $this.val();
 			if(e.keyCode < 48 || (e.keyCode > 57 && e.keyCode < 96) || e.keyCode > 105) {
