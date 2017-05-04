fa4740354fcec86a3eaeab8c9609152456586880
Daniel Rieck <danielcrieck@gmail.com>
2017-05-04 16:00:55 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -986,7 +986,64 @@
     }
 	
 })(jQuery);
-/**
+/***************************************************************************************************************************
+ * jQuery.oue-animate.js: custom JavaScript code to be used on all WSU Undergraduate Education websites for animating      *
+ * elements.                                                                                                               *
+ ***************************************************************************************************************************/
+"use strict";
+
+(function ($) {
+	/*******************************************************************************************************************
+	 * WINDOW LOAD event bindings                                                                                      *
+	 *******************************************************************************************************************/
+    $(window).on("load", function () {
+		var argsList = new Object(); // List of arguments that will be passed to functions
+		var args;
+		
+		// Set up organized list of arguments to be passed to functions called after the window has loaded
+		argsList.doFadeInFromTopAnimations = {
+			slctrAnimatedElems: "js-fade-in-from-top",
+			dfltSpeed: 1000,
+			fastClass: "fast",
+			fastModifier: 0.5
+			delayedClass: "delayed",
+		};
+		
+		// Call post window loading functions
+		args = argsList.doFadeInFromTopAnimations
+		doFadeInFromTopAnimations(
+			args.slctrAnimatedElems,
+			args.dfltSpeed,
+			args.fastClass,
+			args.fastModifier,
+			args.delayedClass
+		);
+	});
+	
+	/*******************************************************************************************************************
+	 * POST WINDOW LOADING FUNCTIONS                                                                                   * 
+	 *******************************************************************************************************************/
+	function doFadeInFromTopAnimations(slctrAnimatedElems, dfltSpeed, fastClass, fastModifier, delayedClass) {
+		var $objs = $(slctrAnimatedElems);
+		$objs.each(function(){
+			var $this = $(this);
+			var height = $this.height;
+			$this.css("top", -height);
+			var speed = dfltSpeed;
+			var delayTime = 0;
+			if ($this.hasClass(fastClass)) {
+				speed *= fastModifier;
+			}
+			if ($this.hasClass(delayedClass)) {
+				delayTime = speed;
+			}
+			$this.stop().delay(delayTime).animate({
+				opacity: 1,
+				top: 0
+			}, speed);
+		});
+	}
+})(jQuery);/**
  * jQuery.textResize.js
  * Released under GNU GPLv2
  *
