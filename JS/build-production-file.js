c4bc67bd7629e7e6b2d0c44bf08393818c400049
Daniel Rieck <danielcrieck@gmail.com>
2017-05-05 15:20:03 -0700

    Adding new file to build order
    File added: javascript-based animation script for OUE websites

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -3,6 +3,7 @@ var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files'
 concat([
  './ugr-custom.js',
  '../WSU-UE---JS/jQuery.oue-custom.js',
+ '../WSU-UE---JS/jQuery.oue-animate.js',
  '../WSU-UE---JS/jQuery.textResize.js',
  '../WSU-UE---JS/jQuery.forms.js',
  '../WSU-UE---JS/jQuery.are-you-sure.js',
