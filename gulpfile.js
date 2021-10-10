007f4de054d002762b2f17299d227b117bbcf675
invokeImmediately <danielcrieck@gmail.com>
2021-10-09 17:27:14 -0700

    Refactor: Update settings for custom CSS builds
    In gulpfile.js, update the build settings for importing fonts and inserting the comment block that documents the media queries section in builds of custom CSS stylesheets.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
  * Gulp automation task definition file for setting up tasks that build CSS and JS files for use on
  *   the WSUWP website of the Office of Undergraduate Research.
  *
- * @version 1.0.0
+ * @version 1.0.1
  *
  * @link https://github.com/invokeImmediately/undergraduateresearch.wsu.edu/blob/master/gulpfile.js
  * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
@@ -31,8 +31,8 @@
 // §1: Gulp task dependencies...................................................................43
 // §2: Specification of build settings .........................................................48
 //   §2.1: getCssBuildSettings()................................................................51
-//   §2.2: getJsBuildSettings()................................................................129
-// §3: Entry point: Set up of build tasks......................................................160
+//   §2.2: getJsBuildSettings().................................................................76
+// §3: Entry point: Set up of build tasks......................................................107
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ( function() {
@@ -60,63 +60,10 @@ function getCssBuildSettings() {
 		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
 		dependenciesPath: './WSU-DAESA-CSS/',
 		destFolder: './CSS/',
-		fontImportStr: '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,' +
-			'400i,600,600i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700i|Robot' +
-			'o+Mono:400,400i,700,700i&display=swap\');\r\n',
+		fontImportStr: gulpBuilder.getDaesaFontImportStr(),
 		insertingMediaQuerySectionHeader: {
 			before: /^@media/,
-			lineBefore:  '/*! ========================================================================' +
-						'========================\r\n' +
-					'*** ▐▀▄▀▌█▀▀▀ █▀▀▄ ▀█▀ ▄▀▀▄   ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ ▀█▀ █▀▀▀ ▄▀▀▀   ▄▀▀▀ █▀▀▀ ▄▀▀▀▐▀█▀▌' +
-						'▀█▀ ▄▀▀▄ ▐▀▀▄\r\n' +
-					'*** █ ▀ ▌█▀▀  █  █  █  █▄▄█   █  █ █  █ █▀▀  █▄▄▀  █  █▀▀  ▀▀▀█   ▀▀▀█ █▀▀  █     █  ' +
-						' █  █  █ █  ▐\r\n' +
-					'*** █   ▀▀▀▀▀ ▀▀▀  ▀▀▀ █  ▀    ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▀▀▀ ▀▀▀▀ ▀▀▀    ▀▀▀  ▀▀▀▀  ▀▀▀  █  ' +
-						'▀▀▀  ▀▀  ▀  ▐\r\n' +
-					'*** ==================================================================================' +
-						'==============\r\n' +
-					'*** Media queries built from precompiled CSS written in the Less language extension' +
-					  ' of CSS. Queries\r\n' +
-					'***   in this section are a combination of those designed for use on DAESA websites' +
-						' and those\r\n' +
-					'***   intended specifically for use on the Office of Undergraduate Research' +
-						' website.\r\n' +
-					'***\r\n' +
-					'*** Fully documented, precompiled source code from which this section of stylesheet' +
-						' was developed\r\n' +
-					'***   is maintained on the following two GitHub projects:\r\n' +
-					'***   - https://github.com/invokeImmediately/WSU-DAESA-CSS/\r\n' +
-					'***   - https://github.com/invokeImmediately/undergraduateresearch.wsu.edu/\r\n' +
-					'***\r\n' +
-					'*** @author Daniel Rieck [daniel.rieck@wsu.edu]' +
-						' (https://github.com/invokeImmediately)\r\n' +
-					'*** @license: MIT - Copyright (c) 2021 Washington State University\r\n' +
-					'***   Permission is hereby granted, free of charge, to any person obtaining a copy of' +
-						' this software\r\n' +
-					'***     and associated documentation files (the "Software"), to deal in the Software' +
-						' without\r\n' +
-					'***     restriction, including without limitation the rights to use, copy, modify,' +
-						' merge, publish,\r\n' +
-					'***     distribute, sublicense, and/or sell copies of the Software, and to permit' +
-						' persons to whom\r\n' +
-					'***     the Software is furnished to do so, subject to the following conditions:\r\n' +
-					'***   The above copyright notice and this permission notice shall be included in all' +
-						' copies or\r\n' +
-					'***     substantial portions of the Software.\r\n' +
-					'***   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR' +
-						' IMPLIED, INCLUDING\r\n' +
-					'***     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR' +
-						' PURPOSE AND\r\n' +
-					'***     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE' +
-						' FOR ANY\r\n' +
-					'***     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR' +
-						' OTHERWISE,\r\n' +
-					'***     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER' +
-						' DEALINGS IN THE\r\n' +
-					'***     SOFTWARE.\r\n' +
-					'*** ==================================================================================' +
-						'==============\r\n' +
-					'**/',
+			lineBefore:  '/*! ================================================================================================\r\n*** ▐▀▄▀▌█▀▀▀ █▀▀▄ ▀█▀ ▄▀▀▄   ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ ▀█▀ █▀▀▀ ▄▀▀▀   ▄▀▀▀ █▀▀▀ ▄▀▀▀▐▀█▀▌▀█▀ ▄▀▀▄ ▐▀▀▄\r\n*** █ ▀ ▌█▀▀  █  █  █  █▄▄█   █  █ █  █ █▀▀  █▄▄▀  █  █▀▀  ▀▀▀█   ▀▀▀█ █▀▀  █     █   █  █  █ █  ▐\r\n*** █   ▀▀▀▀▀ ▀▀▀  ▀▀▀ █  ▀    ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▀▀▀ ▀▀▀▀ ▀▀▀    ▀▀▀  ▀▀▀▀  ▀▀▀  █  ▀▀▀  ▀▀  ▀  ▐\r\n*** ================================================================================================\r\n*** Media queries built from precompiled CSS written in the Less language extension of CSS. Queries\r\n***   in this section are a combination of those designed for use on DAESA websites and those\r\n***   intended specifically for use on the Office of Undergraduate Research website.\r\n***\r\n*** Fully documented, precompiled source code from which this section of stylesheet was developed\r\n***   is maintained on the following two GitHub projects:\r\n***   - https://github.com/invokeImmediately/WSU-DAESA-CSS/\r\n***   - https://github.com/invokeImmediately/undergraduateresearch.wsu.edu/\r\n***\r\n*** @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)\r\n*** @license: MIT - Copyright (c) 2021 Washington State University\r\n***   Permission is hereby granted, free of charge, to any person obtaining a copy of this software\r\n***     and associated documentation files (the "Software"), to deal in the Software without\r\n***     restriction, including without limitation the rights to use, copy, modify, merge, publish,\r\n***     distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom\r\n***     the Software is furnished to do so, subject to the following conditions:\r\n***   The above copyright notice and this permission notice shall be included in all copies or\r\n***     substantial portions of the Software.\r\n***   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING\r\n***     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\r\n***     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY\r\n***     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\r\n***     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\n***     SOFTWARE.\r\n*** ================================================================================================\r\n**/',
 			stopAfterFirstMatch: true
 		},
 		minCssFileExtension: '.min.css',
